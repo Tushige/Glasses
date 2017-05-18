f5066a676edb6d7687d6d393aa63b3e83f80bdaa
Tushig <tushige2000@gmail.com>
2017-05-18 14:44:13 -0500

    made homepage intro content responsive

--- src/views/Home/Homepage.js
+++ src/views/Home/Homepage.js
@@ -1,38 +1,32 @@
-/*******************************************************************************
- * This file contains the component that describes the app homepage
+/*******************************************************************************\
+ * @file Homepage.js
+ *
+ * Contains the component that describes the app homepage for anonymous users
  *******************************************************************************/
 import React, { Component } from 'react';
 import './Homepage.css';
 import {Link} from 'react-router-dom';
-import {
-    Form,
-    FormControl,
-    FormGroup,
-    Button,
-    Col,
+import {Button,
 } from 'react-bootstrap';
 
 function SignupBtn(props) {
     return (
-        <Button id="signup-btn"
+        <Button id="homepage-signup-btn"
             type="submit"
-            bsStyle="success"
-            block>
-            <Link to='/signup'>Sign Up for Free</Link>
+            bsStyle="success">
+            <Link to='/signup'>SIGN UP FOR FREE</Link>
         </Button>
     )
 }
 class Homepage extends Component {
     render() {
         return (
-            <div id="homepage-body">
+            <div id="homepage-content">
                 <div id="intro">
-                    <h1> Keep your reading list in one place.</h1>
-                    <p>Inspiration strikes anywhere. Glasses lets you capture</p>
-                    <p>nurture, and share your ideas across any device.</p>
-                    <div id="signup-btn-container">
-                        <SignupBtn/>
-                    </div>
+                    <h1 id=""> Keep your reading list in one place.</h1>
+                    <p>Inspiration strikes anywhere. Glasses lets you capture
+                    nurture, and share your ideas across any device.</p>
+                    <SignupBtn/>
                 </div>
             </div>
         )
